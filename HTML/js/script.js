fca9abea0744cabbcba96cc4741ea798a2c6304b
guymenten <guymenten@yahoo.com>
2020-10-06 00:00:45 +0200

    Batch update

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-accueil","displayName":"Accueil","link":{"linkType":"LinkTypePage","href":"#!page-accueil"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-mesures","displayName":"Mesures","link":{"linkType":"LinkTypePage","href":"#!page-mesures"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-accueil","displayName":"Accueil","link":{"linkType":"LinkTypePage","href":"#!page-accueil"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-mesures","displayName":"Mesures","link":{"linkType":"LinkTypePage","href":"#!page-mesures"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
