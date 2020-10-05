98f6127bcb7e2c8f81f1523bf7878bf40a4112f1
guymenten <guymenten@yahoo.com>
2020-10-05 23:39:36 +0200

    Batch update

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-accueil","displayName":"Accueil","link":{"linkType":"LinkTypePage","href":"#!page-accueil"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-mesures","displayName":"Mesures","link":{"linkType":"LinkTypePage","href":"#!page-mesures"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-accueil","displayName":"Accueil","link":{"linkType":"LinkTypePage","href":"#!page-accueil"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-mesures","displayName":"Mesures","link":{"linkType":"LinkTypePage","href":"#!page-mesures"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-test","displayName":"Test","link":{"linkType":"LinkTypePage","href":"#!page-test"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
